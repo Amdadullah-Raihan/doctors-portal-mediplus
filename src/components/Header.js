cef5818e053dd13e6cd929e9e77c3dbea7e7476a
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2022-12-19 21:08:37 +0600

    routes added on navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../images/logo.png'
 
 const Header = () => {
+
+    let activeStyle = {
+        textDecoration: "underline",
+        color:"blue"
+    };
+    let activeClassName = "underline";
+
+
     return (
-        <Navbar expand='lg'>
+        <Navbar expand='lg' className='navbar-section '>
             <Container>
                 <Navbar.Brand>
                     <img src={logo} height='30px' alt="" />
@@ -13,9 +21,38 @@ const Header = () => {
                 <Navbar.Toggle aria-controls='responsive-navbar-nav'></Navbar.Toggle>
                 <Navbar.Collapse id='responsive-navbar-nav'>
                     <Nav className='me-auto'>
-                        <Nav.Link>Home</Nav.Link>
-                        <Nav.Link>Doctors</Nav.Link>
-                        <Nav.Link>Services</Nav.Link>
+                        <Nav.Link>
+                            <NavLink
+                                to="/"
+                                style={({ isActive }) =>
+                                    isActive ? activeStyle : undefined
+                                }
+                            >
+                                Home
+                            </NavLink>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <NavLink
+                                to="/doctors"
+                                style={({ isActive }) =>
+                                    isActive ? activeStyle : undefined
+                                }
+                            >
+                               Doctors
+                            </NavLink>
+                        </Nav.Link>
+
+                        <Nav.Link>
+                            <NavLink
+                                to="/services"
+                                style={({ isActive }) =>
+                                    isActive ? activeStyle : undefined
+                                }
+                            >
+                                Services
+                            </NavLink>
+                        </Nav.Link>
+
 
 
                     </Nav>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
     return (
         <div className='d-flex flex-column align-items-center justify-content-center' style={{height:"90vh"}}>
-            <p style={{color:"blue", fontSize:"100px", textShadow:"2px 2px 2px"}}>404!</p>
+            <p className='text-danger fs-xxl' style={{ fontSize:"100px", textShadow:"5px 2px 10px"}}>404!</p>
             <h1>Oopps!! The page you're looking for was not found </h1>
             <Link to='/'><button className='btn btn-danger text-bold'>Go to home</button></Link>
         </div>
